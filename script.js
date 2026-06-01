4fea1fd2ca80083ff15d31e9b2ffcc3be075e255
HeyImJoella <joelle.iris@gmail.com>
2026-06-01 21:43:56 +0200

    Roos heeft aandacht nodig

--- script.js
+++ script.js
@@ -13,44 +13,36 @@ function getComputerChoice() {
     return choices[randomIndex];
 };
 
+let playerScore = 0;
+let computerScore = 0;
+
 function playRound(playerChoice){
     const computerChoice = getComputerChoice();
 
     if (playerChoice === computerChoice){
-        return "It's a tie!";
+        return showResults.innerText = "It's a tie!";
     }else if (playerChoice === "rock" && computerChoice === "paper"){
-        return showResults.innerText = "Computer wins! Paper wraps rock.";
+        computerScore++;
+        showResults.innerText = "Computer wins! Paper wraps rock.";
     }else if (playerChoice === "rock" && computerChoice === "scissors"){
-        return showResults.innerText = "Player wins! Rock smashes scissors.";
+        playerScore++;
+        showResults.innerText = "Player wins! Rock smashes scissors.";
     }else if (playerChoice === "paper" && computerChoice === "rock"){
-        return showResults.innerText = "Player wins! Paper wraps rock.";
+        playerScore++;
+        showResults.innerText = "Player wins! Paper wraps rock.";
     }else if (playerChoice === "paper" && computerChoice === "scissors"){
-        return showResults.innerText = "Computer wins! Scissors cut paper.";
+        computerScore++;
+        showResults.innerText = "Computer wins! Scissors cut paper.";
     }else if (playerChoice === "scissors" && computerChoice === "rock"){
-        return showResults.innerText = "Computer wins! Rock smashes scissors.";
+        computerScore++;
+        showResults.innerText = "Computer wins! Rock smashes scissors.";
     }else if(playerChoice === "scissors" && computerChoice === "paper"){
-        return showResults.innerText = "Player wins! Scissors cut paper.";
+        playerScore++;
+        showResults.innerText = "Player wins! Scissors cut paper.";
     }else{
-        return showResults.innerText = "No weapons were chosen.";
+        showResults.innerText = "No weapons were chosen.";
     }
+
+    console.log(computerScore);
+    console.log(playerScore);
 };
-
-
-
-
-
-
-
-
-//We gaan knoppen ophalen
-//We gaan de mens keuze opties geven en daaruit iets laten kiezen
-//We gaan de knoppen waardes geven
-//We gaan de computer een random keuze laten maken
-//We gaan meten wie de winaar is
-//We gaan een resultaat meegeven per ronde en uitleg geven waarom iemand wint
-//We gaan de eindscore van 3 bijhouden
-//We gaan een reset knop maken
-
-
-
-// prompt("What weapon do you pick? Rock, Paper or Scissors? >:)"); 
