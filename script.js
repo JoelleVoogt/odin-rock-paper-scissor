6c44ed74edc4148b7887e0215658cf41e0ab5c3c
HeyImJoella <joelle.iris@gmail.com>
2026-06-01 21:14:09 +0200

    Added javascript functionality

--- script.js
+++ script.js
@@ -2,20 +2,40 @@ const rockBtn = document.getElementById('rock');
 const paperBtn = document.getElementById('paper');
 const scissorsBtn = document.getElementById('scissors');
 
-rockBtn.addEventListener("click", () => (playRound("Rock")));
-paperBtn.addEventListener("click", () => (playRound("Paper")));
-scissorsBtn.addEventListener("click", () => (playRound("Scissors")));
-
-function playRound(choice){
-    if 
-}
-
+rockBtn.addEventListener("click", () => (playRound("rock")));
+paperBtn.addEventListener("click", () => (playRound("paper")));
+scissorsBtn.addEventListener("click", () => (playRound("scissors")));
 
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 };
+
+function playRound(playerChoice){
+    const computerChoice = getComputerChoice();
+
+    if (playerChoice === computerChoice){
+        return "It's a tie!";
+    }else if (playerChoice === "rock" && computerChoice === "paper"){
+        return "Computer wins! Paper wraps rock."
+    }else if (playerChoice === "rock" && computerChoice === "scissors"){
+        return "Player wins! Rock smashes scissors."
+    }else if (playerChoice === "paper" && computerChoice === "rock"){
+        return "Player wins! Paper wraps rock."
+    }else if (playerChoice === "paper" && computerChoice === "scissors"){
+        return "Computer wins! Scissors cut paper."
+    }else if (playerChoice === "scissors" && computerChoice === "rock"){
+        return "Computer wins! Rock smashes scissors."
+    }else if(playerChoice === "scissors" && computerChoice === "paper"){
+        return "Player wins! Scissors cut paper."
+    }else{
+        return "No weapons were chosen."
+    }
+};
+
+
+
 
 
 
